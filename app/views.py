965049ac1d162bab33a22cf1a283b098172d13ee
kearney <191615342@qq.com>
2021-04-26 16:07:26 +0800

    使用session来进行用户认证

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 # -*- encoding: utf-8 -*-
 from app import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session, flash
 from app.forms import LoginForm
+from app.models import User
 
 
 @app.route('/')
@@ -13,7 +14,27 @@ def index():
 def login():
     form = LoginForm()
     if request.method == 'POST' and form.validate_on_submit():
+        '''验证表单'''
         username = form.username.data
         password = form.password.data
-        return redirect(url_for('index'))
+        print(username,password)
+        user = User.query.filter(User.username==username, User.password==password).first()
+        print(user)
+        if user:
+            session['AUTH'] = username
+        return redirect(url_for('isauth'))
+    if 'AUTH' in session:    
+        session.pop('AUTH')
     return render_template('login.html', form=form)
+
+
+@app.route('/auth')
+def isauth():
+    '''
+    是否登陆测试页面
+    已登陆： 显示'已成功登陆'
+    未登陆： 重定向到登陆页面
+    '''
+    if 'AUTH' in session:
+        return '已成功登陆'+session['AUTH']
+    return redirect(url_for('login'))
