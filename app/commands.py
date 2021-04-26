c3eea16401ab362da9d99fd8c29ecb5c97ac381b
kearney <191615342@qq.com>
2021-04-26 14:56:37 +0800

    添加faker生成测试虚拟数据命令

--- app/commands.py
+++ app/commands.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 import click
 from app import app, db
-from app.models import Message
+from app.models import Message, User
 
 
 @app.cli.command()
@@ -12,3 +12,36 @@ def initdb():
     """   
     db.create_all()
     click.echo('Initialized database.')
+
+
+@app.cli.command()
+@click.option('--count', default=20, help='Quantity of messages, default is 20.')
+def forge(count):
+    """
+    向数据库中添加虚假数据，测试时使用
+    使用方法： flask forge
+    """  
+    from faker import Faker  # noqa
+
+    # db.drop_all()
+    # db.create_all()
+
+    fake = Faker()
+    click.echo('Working...')
+
+    for i in range(count):
+        message = Message(
+            name=fake.name(),
+            body=fake.sentence(),
+            timestamp=fake.date_time_this_year()
+        )
+        user = User(
+            id=i,
+            username=fake.name(),
+            password='123456'
+        )
+        db.session.add(user)
+        db.session.add(message)
+
+    db.session.commit()
+    click.echo('Created %d fake messages.' % count)
--- app/models.py
+++ app/models.py
@@ -9,3 +9,9 @@ class Message(db.Model):
     name = db.Column(db.String(20))
     body = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, index=True)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    password = db.Column(db.String(128))
